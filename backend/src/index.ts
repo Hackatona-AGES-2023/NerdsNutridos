ad89d6b8e77f06f0a96b2ec8f9d9f5136a39365d
BrunoPanizzi <brunopanizziq@gmail.com>
2023-05-27 13:15:12 -0300

    backend done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,26 @@
 import express from 'express'
+import cors from 'cors'
 
 import { saveToJSON } from './saveToJson'
 import { getUserFromJson } from './getUserFromJson'
 
 import { generateUid } from './utils/generateUid'
-import { resumeConversation } from './services/gpt'
+import { extendData, resumeConversation } from './services/gpt'
 
 const app = express()
+
+app.use(cors())
+
 app.use(express.json())
 
 const port = 3000
 
 app.post('/createUser', async (req, res) => {
-  const { conversation } = req.body
+  console.log('criando novo usuario')
+
+  const conversation = req.headers.conversation as string
+
+  console.log('this is so bad: ', conversation)
 
   if (!conversation) {
     return res.status(400).send('Missing conversation')
@@ -26,12 +34,13 @@ app.post('/createUser', async (req, res) => {
     return res.status(500).send('Error on resume conversation')
   }
 
-  const json = JSON.parse(resume) as Record<string, unknown>
+  const data = JSON.parse(resume) as Record<string, unknown>
 
-  saveToJSON(uid, json)
+  saveToJSON(uid, data)
 
   return res.json({
     uid,
+    data,
   })
 })
 
@@ -50,6 +59,36 @@ app.get('/user/:uid', (req, res) => {
   }
 
   return res.json(user)
+})
+
+app.post('/updateUser/:uid', async (req, res) => {
+  const conversation = req.headers.conversation as string
+
+  console.log('this is so bad updating: ', conversation)
+
+  const uid = req.params.uid
+
+  if (!uid) {
+    return res.status(400).send('Missing uid')
+  }
+
+  if (!conversation) {
+    return res.status(400).send('Missing conversation')
+  }
+
+  const newResume = await extendData(uid, conversation)
+
+  if (!newResume) {
+    return res.status(500).send('Error on extend data')
+  }
+
+  console.log('new resume: ', newResume)
+
+  const json = JSON.parse(newResume) as Record<string, unknown>
+
+  saveToJSON(uid, json)
+
+  return res.json(json)
 })
 
 app.listen(port, () => {
--- backend/src/services/gpt.ts
+++ backend/src/services/gpt.ts
@@ -1,7 +1,8 @@
 import { Configuration, OpenAIApi } from 'openai'
+import { getUserFromJson } from '../getUserFromJson'
 
 const configuration = new Configuration({
-  apiKey: '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',
+  apiKey: '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',
 })
 const openai = new OpenAIApi(configuration)
 
@@ -12,6 +13,31 @@ export const resumeConversation = async (conversation: string) => {
       Com base na seguinte conversa, crie um objeto json com tudo que sabe sobre o cliente. 
 
       ${conversation}
+    `,
+    temperature: 0.7,
+    max_tokens: 500,
+    top_p: 1.0,
+    frequency_penalty: 0.0,
+    presence_penalty: 0.0,
+  })
+
+  return response.data.choices[0].text
+}
+
+export const extendData = async (uid: string, convesation: string) => {
+  const data = getUserFromJson(uid)
+
+  const response = await openai.createCompletion({
+    model: 'text-davinci-003',
+    prompt: `
+      A mensagem a seguir contêm um JSON com as informações do usuário, e também uma conversa do usuário com o bot, extenda as informações do JSON com base na conversa. 
+      Devolva APENAS o objeto JSON.
+
+      JSON: 
+      ${JSON.stringify(data!.data)}
+
+      Conversa:
+      ${convesation}
     `,
     temperature: 0.7,
     max_tokens: 500,
